dce5ed14f3a7cb308ae5dbd70a75a56a7eb601e5
Jeannette <114934882+jeannette-1@users.noreply.github.com>
2023-12-27 10:49:28 +0300

    Update utils.py

--- mlops_project/utils.py
+++ mlops_project/utils.py
@@ -5,16 +5,17 @@ from sklearn.preprocessing import OneHotEncoder
 
 def load_dataset(data_path):
     data = pd.read_csv(data_path)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    return X_train, X_test, y_train, y_test
+    return data
 
 def encode(data):
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
     ohe = OneHotEncoder(sparse=False)
-    neighborhood_encoded = ohe.fit_transform(df[["Neighborhood"]])
+    neighborhood_encoded = ohe.fit_transform(data[["Neighborhood"]])
     neighborhood_encoded_cols = [f"Neighborhood_{category}" for category in ohe.categories_[0]]
     neighborhood_df = pd.DataFrame(neighborhood_encoded, columns=neighborhood_encoded_cols)
-    df_encoded = pd.concat([df.drop("Neighborhood", axis=1), neighborhood_df], axis=1)
-    X_train = df_encoded.drop("Price", axis=1)
+    df_encoded = pd.concat([data.drop("Neighborhood", axis=1), neighborhood_df], axis=1)
+    x_train = df_encoded.drop("Price", axis=1)
     y_train = df_encoded["Price"]
-    return X_train, X_test, y_train, y_test
+    return x_train, x_test, y_train, y_test
     
